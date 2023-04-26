bfc55d02eb090cbf9bc3a4aba3b83edbd02a7a81
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-26 10:23:23 +0300

    tusk dune

--- Python_12.04.2023.py
+++ Python_12.04.2023.py
@@ -1,1 +1,11 @@
+try:
 
+    numbers = [1, 2, 3, 4, 5]
+    def average(numbers):
+        if len(numbers) == 0:
+            return 0  
+        return sum(numbers) / len(numbers)
+    print(average(numbers))
+    
+except Exception as e:
+    print(e)
