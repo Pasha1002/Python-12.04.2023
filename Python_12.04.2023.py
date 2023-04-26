01950f3ba64f010368863b3eda8f53fd09fc8df0
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-26 16:50:24 +0300

    tusk dune

--- Python_12.04.2023.py
+++ Python_12.04.2023.py
@@ -1,1 +1,18 @@
 
+try:
+
+    import random
+    list = [random.randint(0,10) for i in range(20)]
+    print(list)
+    def the_number_that_occurs_most_often(list):
+        counts = {}
+        for num in list:
+            if num in counts:
+                counts[num] += 1
+            else:
+                counts[num] = 1
+        return max(counts, key=lambda x: (counts[x], -x))
+    print(the_number_that_occurs_most_often(list))
+
+except Exception as e:
+    print(e)
