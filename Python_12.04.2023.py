aff2f0c0264be19961fe51fac7581b32e95b18a8
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-26 10:42:37 +0300

    tusk dune

--- Python_12.04.2023.py
+++ Python_12.04.2023.py
@@ -1,1 +1,13 @@
 
+try:
+
+
+    import string
+    def capitalize_words(string):
+        return string.title()
+    string = input('Enter a string of words: ')
+    print (capitalize_words(string))
+
+except Exception as e:
+    print(e)
+
