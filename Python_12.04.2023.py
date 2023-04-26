1ac0d93e0b286f959b8c2e3c6384a80fa2afd812
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-26 10:57:35 +0300

    tusk done

--- Python_12.04.2023.py
+++ Python_12.04.2023.py
@@ -1,1 +1,16 @@
 
+try:
+
+    import random
+    list=[random.randint(0,10) for i in range(5)]
+    list2=[random.randint(0,10 ) for i in range(5)]
+    print(list,list2)
+
+    def scalar(list,list2):
+        scalar = sum([list[i]*list2[i] for i in range(len(list))])
+        return(scalar)
+    print(scalar(list,list2))
+
+except Exception as e:
+    print(e)
+
