836171fe8bfc2172e0f87b7b03cf7735f08266ff
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-26 16:43:17 +0300

    tusk dune

--- Python_12.04.2023.py
+++ Python_12.04.2023.py
@@ -1,1 +1,13 @@
 
+try:
+
+    string = (input("Введіть рядок: "))
+    def output_first_and_last_three_chars(string):
+        if len(string) < 6:
+            return "Недостатньо символів"
+        else:
+            return string[:3] + string[-3:]
+    print(output_first_and_last_three_chars(string))
+    
+except Exception as e:
+    print(e)
