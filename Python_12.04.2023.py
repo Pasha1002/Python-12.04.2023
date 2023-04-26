f90d119c85bbe9345aa49f082db17f2ec6d601f4
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-26 11:10:15 +0300

    tusk dune

--- Python_12.04.2023.py
+++ Python_12.04.2023.py
@@ -1,1 +1,14 @@
 
+
+try:
+
+    n = int(input('Enter number: '))
+    def delitel(n):
+        for i in range(1,int(n/2)+1):
+            if n%i == 0:
+                yield i
+    print('Divisors of a number',list(delitel(n)))
+
+except Exception as e:
+    print(e)
+
