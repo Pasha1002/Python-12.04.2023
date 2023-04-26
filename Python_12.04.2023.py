58074c64f12f0ee7a45043ac869e8c4e504f43ed
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-26 10:29:18 +0300

    tusk dune

--- Python_12.04.2023.py
+++ Python_12.04.2023.py
@@ -1,1 +1,18 @@
 
+
+try:
+
+
+
+    n = int(input("Enter a number: "))
+    def fib(n):   
+        if n in (1, 2):
+            return 1
+        
+        return fib(n-1) + fib(n-2)
+    print(fib(n))
+       
+
+except Exception as e:
+    print(e)
+
