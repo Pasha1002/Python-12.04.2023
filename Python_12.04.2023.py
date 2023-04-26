098f0b2cfe45afa332f4ec5ded852f53e96d4112
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-26 16:33:08 +0300

    tusk dune

--- Python_12.04.2023.py
+++ Python_12.04.2023.py
@@ -1,1 +1,19 @@
 
+try:
+
+    a = input('Enter name: ')
+    b = int(input('Enter age: '))
+    a1 = input('Enter name: ')
+    b1 = int(input('Enter age: '))
+    a2 = input('Enter name: ')
+    b2 = int(input('Enter age: '))
+    list = [(a,b),(a1,b1),(a2,b2)]
+    def sort_age(list):
+        list.sort(key=lambda x: x[1])
+        return(list)
+    print(sort_age(list))
+
+
+except Exception as e:
+    print(e)
+
